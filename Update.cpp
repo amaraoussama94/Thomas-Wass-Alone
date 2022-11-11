5e42a0fa651293530d375789391f224900695f05
amaraoussama94 <wassa36@yahoo.fr>
2022-11-11 12:00:18 +0100

    update while starting new level

--- Update.cpp
+++ Update.cpp
@@ -6,6 +6,18 @@
 
 void Engine::update(float dtAsSeconds)
 {
+    if (m_NewLevelRequired)
+    {
+        // These calls to spawn will be moved to a new
+        // loadLevel() function soon
+        // Spawn Thomas and Bob
+        m_Thomas.spawn(Vector2f(0,0), GRAVITY);
+        m_Bob.spawn(Vector2f(100, 0), GRAVITY);
+        // Make sure spawn is called only once
+        m_TimeRemaining = 10;
+        m_NewLevelRequired = false;
+    }
+
     if (m_Playing)
     {
         // Count down the time the player has left
