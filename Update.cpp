2de0458ed39814afa170689a6d8a47f6845ee06f
amaraoussama94 <wassa36@yahoo.fr>
2022-11-11 12:02:18 +0100

    Update th tom and bob caractere and set view

--- Update.cpp
+++ Update.cpp
@@ -20,6 +20,10 @@ void Engine::update(float dtAsSeconds)
 
     if (m_Playing)
     {
+        // Update Thomas
+        m_Thomas.update(dtAsSeconds);
+        // Update Bob
+        m_Bob.update(dtAsSeconds);
         // Count down the time the player has left
         m_TimeRemaining -= dtAsSeconds;
         // Have Thomas and Bob run out of time?
@@ -28,4 +32,22 @@ void Engine::update(float dtAsSeconds)
             m_NewLevelRequired = true;
         }
     }// End if playing
+    // Set the appropriate view around the appropriate character
+    if (m_SplitScreen)
+    {
+        m_LeftView.setCenter(m_Thomas.getCenter());
+        m_RightView.setCenter(m_Bob.getCenter());
+    }
+    else
+    {
+        // Centre full screen around appropriate character
+        if (m_Character1)
+        {
+            m_MainView.setCenter(m_Thomas.getCenter());
+        }
+        else
+        {
+            m_MainView.setCenter(m_Bob.getCenter());
+        }
+    }
 }
