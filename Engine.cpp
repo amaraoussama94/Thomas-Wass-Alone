ca549b74b13531533ac9e3944137be6441caac66
amaraoussama94 <wassa36@yahoo.fr>
2022-11-11 15:04:30 +0100

    update engine constructeur to load titles sheet

--- Engine.cpp
+++ Engine.cpp
@@ -19,6 +19,8 @@ Engine::Engine()
     m_BackgroundTexture = TextureHolder::GetTexture("graphics/background.png");
     // Associate the sprite with the texture
     m_BackgroundSprite.setTexture(m_BackgroundTexture);
+    // Load the texture for the background vertex array 
+    m_TextureTiles = TextureHolder::GetTexture("graphics/tiles_sheet.png");
 }
 
 void Engine::run()
