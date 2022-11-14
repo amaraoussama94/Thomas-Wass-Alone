b8b43db24066c930eda41f910f5653807df31a94
amaraoussama94 <wassa36@yahoo.fr>
2022-11-14 10:45:07 +0100

    all bug fixzd game work

--- Thomas.cpp
+++ Thomas.cpp
@@ -3,47 +3,54 @@
 
 Thomas::Thomas()
 {
-    // Associate a texture with the sprite
-    m_Sprite = Sprite(TextureHolder::GetTexture("graphics/thomas.png"));
-    m_JumpDuration = .45;//nearly half a second
-}
+	// Associate a texture with the sprite
+	m_Sprite = Sprite(TextureHolder::GetTexture(
+		"graphics/thomas.png"));
 
+	m_JumpDuration = .45;//nearly half a second
+}
 
 // A virtual function
 bool Thomas::handleInput()
 {
-    m_JustJumped = false;
-    if (Keyboard::isKeyPressed(Keyboard::W))
-    {
-        // Start a jump if not already jumping
-        // but only if standing on a block (not falling)
-        if (!m_IsJumping && !m_IsFalling)
-        {
-            m_IsJumping = true;
-            m_TimeThisJump = 0;
-            m_JustJumped = true;
-        }
-    }
-    else
-    {
-        m_IsJumping = false;
-        m_IsFalling = true;
-    }
-    if (Keyboard::isKeyPressed(Keyboard::A))
-    {
-        m_LeftPressed = true;
-    }
-    else
-    {
-        m_LeftPressed = false;
-    }
-    if (Keyboard::isKeyPressed(Keyboard::D))
-    {
-        m_RightPressed = true;
-    }
-    else
-    {
-        m_RightPressed = false;
-    }
-    return m_JustJumped;//This will let the calling code know if it needs to play a jumping sound effec
+	m_JustJumped = false;
+
+	if (Keyboard::isKeyPressed(Keyboard::W))
+	{
+
+		// Start a jump if not already jumping
+		// but only if standing on a block (not falling)
+		if (!m_IsJumping && !m_IsFalling)
+		{
+			m_IsJumping = true;
+			m_TimeThisJump = 0;
+			m_JustJumped = true;
+		}
+	}
+	else
+	{
+		m_IsJumping = false;
+		m_IsFalling = true;
+
+	}
+	if (Keyboard::isKeyPressed(Keyboard::A))
+	{
+		m_LeftPressed = true;
+	}
+	else
+	{
+		m_LeftPressed = false;
+	}
+
+
+	if (Keyboard::isKeyPressed(Keyboard::D))
+	{
+		m_RightPressed = true;
+	}
+	else
+	{
+		m_RightPressed = false;
+	}
+
+	return m_JustJumped;//This will let the calling code know if it needs to play a jumping sound effec
 }
--- Thomas.hpp
+++ Thomas.hpp
@@ -4,8 +4,10 @@
 class Thomas : public PlayableCharacter
 {
 public:
-    // A constructor specific to Thomas
-    Thomas();
-    // The overridden input handler for Thomas
-    bool virtual handleInput();
+	// A constructor specific to Thomas
+	 Thomas();
+
+	// The overriden input handler for Thomas
+	bool virtual handleInput();
+
 };
